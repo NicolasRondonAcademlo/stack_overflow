c7833ddbc19efb05b0327db226c1873b0b347a1d
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-03-10 18:28:11 -0500

    api

---
+++ api_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+
+class Question(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE
+    )
+
+
+    def __str__(self):
+        return str(self.question)
